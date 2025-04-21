7e6c3b811bf88a1f5fcd274e04a45b9f0eda53c4
Ghgjg <szumiak.bartosz@zs6sobieski.edu.pl>
2025-04-21 15:12:02 +0200

    Dodano SQLite

---
+++ Biblioteka/Laptop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka
+{
+    public class Laptop
+    {
+        public String numSer { get; set; }
+        public String marka { get; set; }
+        public String model { get; set; }
+        public String systemOperacyjny { get; set; }
+        public int iloscSztuk { get; set; }
+
+    }
+}
---
+++ Biblioteka/SQLiteDataAccess.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka
+{
+    public class SQLiteDataAccess
+    {
+        public static List<Laptop> ZaladujLaptopy()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString())) {
+                var output = cnn.Query<Laptop>("select * from Laptopy", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public static void zapiszLaptop(Laptop laptop)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("insert into Laptopy (numSer, marka, model, systemOperacyjny, iloscSztuk) values (@numSer, @marka, @model, @systemOperacyjny, @iloscSztuk)", laptop);
+            }
+            
+        }
+
+
+        private static String LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+    
+}
