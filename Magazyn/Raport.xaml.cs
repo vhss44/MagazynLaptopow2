010910526ef8f7fee2f40910ff2338c1ea2e21bb
Mateusz D <krolpizzy321@gmail.com>
2025-05-07 20:57:23 +0200

    dodanie poczatkow raportu

--- Magazyn/Raport.xaml.cs
+++ Magazyn/Raport.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Biblioteka;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace Magazyn
 {
@@ -19,9 +22,41 @@ namespace Magazyn
     /// </summary>
     public partial class Raport : Window
     {
+        public SeriesCollection SeriesCollection { get; set; }
+        public List<string> Labels { get; set; }
+
         public Raport()
         {
             InitializeComponent();
+            DataContext = this;
+
+            // Pobierz dane z bazy
+            var laptopy = MainWindow.ZaladujLaptopy();
+
+            // 1. Podsumowanie tekstowe
+            txtRaport.Text = $"Raport z dnia: {DateTime.Now}\n\n" +
+                           $"Łączna liczba laptopów: {laptopy.Sum(l => l.IloscSztuk)}\n" +
+                           $"Liczba modeli: {laptopy.Count}\n" +
+                           $"Unikalnych marek: {laptopy.Select(l => l.Marka).Distinct().Count()}";
+
+            // 2. Laptopy na wyczerpaniu (mniej niż 3 sztuki)
+            dgMalolaptop.ItemsSource = laptopy.Where(l => l.IloscSztuk < 3).ToList();
+
+            // 3. Przygotowanie wykresu
+            var daneWykresu = laptopy
+                .GroupBy(l => l.Marka)
+                .Select(g => new { Marka = g.Key, Ilosc = g.Sum(l => l.IloscSztuk) })
+                .OrderByDescending(x => x.Ilosc);
+
+            Labels = daneWykresu.Select(x => x.Marka).ToList();
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "Laptopy",
+                    Values = new ChartValues<int>(daneWykresu.Select(x => x.Ilosc))
+                }
+            };
         }
 
         private void btnS_Click(object sender, RoutedEventArgs e)
